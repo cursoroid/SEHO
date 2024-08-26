ae61f7e2e9cf09e6c1864ccca29eb0b7d480fec1
Prathamesh Mudgale <prathameshmudgale@gmail.com>
2024-08-26 22:47:06 +0530

    project restucture and added a test

---
+++ Tests/check_redis.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+)
+
+func main() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+
+	ctx := context.Background()
+	keys, err := rdb.Keys(ctx, "music:*").Result()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, key := range keys {
+		val, err := rdb.HGetAll(ctx, key).Result()
+		if err != nil {
+			fmt.Printf("Error fetching metadata for %s: %v\n", key, err)
+			continue
+		}
+
+		fmt.Printf("Metadata for %s:\n", key)
+		for field, value := range val {
+			fmt.Printf("  %s: %s\n", field, value)
+		}
+	}
+}
+
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"SEHO/internal/config"
+	"SEHO/internal/music"
+	"SEHO/internal/redis"
+)
+
+func main() {
+	cfg := config.LoadConfig()
+	rdb := redis.InitRedis(cfg.RedisAddress)
+	defer rdb.Close()
+
+	err := music.ScanDirectory(cfg.MusicDirectory, rdb)
+	if err != nil {
+		log.Fatalf("Error scanning music directory: %v", err)
+	}
+
+	log.Println("Music library successfully stored in Redis")
+}
+
